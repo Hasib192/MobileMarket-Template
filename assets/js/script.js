11cb380b5ec178ac6d1bd68c060ab6bfb00b79ac
Hasib <hasibhosen7612@gmail.com>
2024-09-25 20:14:05 +0600

    feat/category slider completed

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 6,
+  spaceBetween: 45,
+  //   autoplay: {
+  //     delay: 2000,
+  //   },
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: ".btn-prev",
+    prevEl: ".btn-next",
+  },
+});
