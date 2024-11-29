75ad2b28bb8030c33c13eaa2c140a1dc5e03c49f
Hasib <hasibhosen7612@gmail.com>
2024-11-29 22:24:54 +0600

    HomePage intregated with Bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,27 @@
 const swiper = new Swiper(".swiper", {
-  slidesPerView: 6,
-  spaceBetween: 45,
+  slidesPerView: 1,
+  spaceBetween: 10,
   //   autoplay: {
   //     delay: 2000,
   //   },
-  
+
   // Navigation arrows
   navigation: {
     nextEl: ".btn-prev",
     prevEl: ".btn-next",
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 45,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 45,
+    },
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 45,
+    },
   },
 });
